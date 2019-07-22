42029cc72afb51b7eeb82e64b2f11c6e89177c18
Jeff Patton <jeffrey@patton-tech.com>
2019-07-22 15:23:35 -0500

    messing around with an overwatch api

---
+++ games/ow.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $.ajax({
+      url: "https://ow-api.com/v1/stats/xbl/sysop71/profile"
+  }).then(function(data) {
+     $('.ow-name').append(data.name);
+     $('.ow-icon').append(data.icon);
+  });
+});
