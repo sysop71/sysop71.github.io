660924194182f2cc9896ce16b7626e0c83c26d62
Jeff Patton <jeffrey@patton-tech.com>
2019-07-22 16:14:27 -0500

    adding more fields

--- games/ow.js
+++ games/ow.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
   }).then(function(data) {
      $('.ow-name').append(data.name);
      $('.ow-icon').append("<img src=" + data.icon + " />");
-     $('.ow-levelIcon').append("<img src=" + data.levelIcon + " />");
+     $('.ow-comp-cards').append(data.competitiveStats.awards.cards);
+     $('.ow-comp-medals').append(data.competitiveStats.awards.medals);
+     $('.ow-comp-played').append(data.competitiveStats.games.played);
+     $('.ow-comp-won').append(data.competitiveStats.games.won);
+     $('.ow-qp-cards').append(data.quickPlayStats.awards.cards);
+     $('.ow-qp-medals').append(data.quickPlayStats.awards.medals);
+     $('.ow-qp-played').append(data.quickPlayStats.games.played);
+     $('.ow-qp-won').append(data.quickPlayStats.games.won);
+     $('.ow-rating').append(data.rating);
+     $('.ow-level').append(data.prestige +""+ data.level);
   });
 });
