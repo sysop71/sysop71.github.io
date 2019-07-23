caaa1ad87d0098a27b474d4d763be6568e0052df
Jeff Patton <jeffrey@patton-tech.com>
2019-07-22 23:26:13 -0500

    removing fluff

--- games/ow.js
+++ games/ow.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
   var currentdate = new Date(); 
-    var datetime = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
   $.ajax({
       url: "https://ow-api.com/v1/stats/xbl/sysop71/profile"
   }).then(function(data) {
