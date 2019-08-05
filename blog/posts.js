85b06826f7bbc171bb02b8caff3bab7619e82093
Jeff Patton <jeffrey@patton-tech.com>
2019-08-05 14:10:05 -0500

    adding date

--- blog/posts.js
+++ blog/posts.js
@@ -4,6 +4,7 @@ $.getJSON(url, function (posts) {
   $.each(posts, function (i, post) {
     $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark' }).text(post.title));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey' }).text(post.subtitle));
+    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-5' }).text(post.date));
     $('.post-content').append($('<p />').text(post.body));
   });
 });
