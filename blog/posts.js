85b51c5ce60523a801b34263e0d750c980544b79
Jeff Patton <jeffrey@patton-tech.com>
2019-08-05 15:52:59 -0500

    something isn't working

--- blog/posts.js
+++ blog/posts.js
@@ -5,8 +5,5 @@ $.getJSON(url, function (posts) {
     $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark' }).text(post.title));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey' }).text(post.subtitle));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-7' }).text(post.date));
-    $.each(post.body.lines, function (i, line) {
-      $('.post-content').append($('<p />').text(line));
-    });
   });
 });
