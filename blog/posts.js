86d939e9cd37cbb52c890e556e2a813c376b307b
Jeff Patton <jeffrey@patton-tech.com>
2019-08-05 16:20:54 -0500

    adjusting formatting

--- blog/posts.js
+++ blog/posts.js
@@ -2,9 +2,9 @@ var url = "https://sysop71.com/blog/posts.json";
 
 $.getJSON(url, function (jobject) {
   $.each(jobject.posts, function (i, post) {
-    $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark' }).text(post.title));
-    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey' }).text(post.subtitle));
-    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-7' }).text(post.date));
+    $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark is-capitalized' }).text(post.title));
+    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-lowercase' }).text(post.subtitle));
+    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-7 is-italic' }).text(post.date));
     $.each(post.body.lines, function (i, line) {
       $('.post-content').append($('<p />').text(line));
     });
