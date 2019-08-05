531c8fe4ccf6f1f7084b6802fd34cdf59dafb918
Jeff Patton <jeffrey@patton-tech.com>
2019-08-05 16:15:24 -0500

    structure matters

--- blog/posts.js
+++ blog/posts.js
@@ -1,7 +1,7 @@
 var url = "https://sysop71.com/blog/posts.json";
 
-$.getJSON(url, function (posts) {
-  $.each(posts, function (i, post) {
+$.getJSON(url, function (jobject) {
+  $.each(jobject.posts, function (i, post) {
     $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark' }).text(post.title));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey' }).text(post.subtitle));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-7' }).text(post.date));
