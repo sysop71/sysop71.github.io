e15f00d5302eccad7f737266f405b2453a541790
Jeff Patton <jeffrey@patton-tech.com>
2019-08-05 16:02:49 -0500

    slight change to check the js is passing along

--- blog/posts.js
+++ blog/posts.js
@@ -4,7 +4,7 @@ $.getJSON(url, function (posts) {
   $.each(posts, function (i, post) {
     $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark' }).text(post.title));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey' }).text(post.subtitle));
-    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-7' }).text(post.date));
+    $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-1' }).text(post.date));
     $.each(post.body.lines, function (i, line) {
       $('.post-content').append($('<p />').text(line));
     });
