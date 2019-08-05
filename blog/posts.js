1acacefdc08c7ac0edc46afa5b7dfb43205451d7
Jeff Patton <jeffrey@patton-tech.com>
2019-08-05 12:57:31 -0500

    tweaking formatting

--- blog/posts.js
+++ blog/posts.js
@@ -2,8 +2,8 @@ var url = "posts.json";
 
 $.getJSON(url, function (posts) {
   $.each(posts, function (i, post) {
-    $('.content').append($('<p />', { 'class': 'title' }).text(post.title));
-    $('.content').append($('<p />', { 'class': 'subtitle' }).text(post.title));
+    $('.content').append($('<p />', { 'class': 'title has-text-grey-dark' }).text(post.title));
+    $('.content').append($('<p />', { 'class': 'subtitle has-text-grey' }).text(post.title));
     $('.content').append($('<p />').text(post.body));
   });
 });
