7053e43adefd8cf27352b61e5e4329d90650a2eb
Jeff Patton <jeffrey@patton-tech.com>
2019-08-06 08:52:40 -0500

    looking at setting anchor tags

--- blog/posts.js
+++ blog/posts.js
@@ -2,7 +2,7 @@ var url = "https://sysop71.com/blog/posts.json";
 
 $.getJSON(url, function (jobject) {
   $.each(jobject.posts, function (i, post) {
-    $('.post-content').append($('<p />', { 'class': 'title has-text-grey-dark is-uppercase' }).text(post.title));
+    $('.post-content').append($('<p id="' + post.date + '" />', { 'class': 'title has-text-grey-dark is-uppercase' }).text(post.title));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-lowercase' }).text(post.subtitle));
     $('.post-content').append($('<p />', { 'class': 'subtitle has-text-grey is-size-7 is-italic' }).text(post.date));
     $.each(post.body.lines, function (i, line) {
